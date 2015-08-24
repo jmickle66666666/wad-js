1390396a542f0b0f9d0c3570ea530d83fee383ea
Jerry Mickle <jerry.mickle@privax.com>
2015-08-24 16:57:30 +0100

    colormap, lineinfo templates

---
+++ colormap.js
@@ -0,0 +1,25 @@
+class Colormap {
+    constructor() {
+        
+    }
+    
+    buildFade(palette = null, fade = [0,0,0]) {
+        
+    }
+    
+    buildInvuln(palette = null, start = [0,0,0], end = [255,255,255]) {
+        
+    }
+    
+    fromLump(lump) {
+        
+    }
+    
+    toLump() {
+        
+    }
+    
+    setPosition(table, index, palIndex) {
+        
+    }
+}
