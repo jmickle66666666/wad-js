5a10ab9cda046755166fb474c0d41a47f3f5aa4c
jmickle <jaymickle@gmail.com>
2015-08-27 12:38:13 +0100

    rewrote wad.js
    its not using es6 any more and its working

--- wad.js
+++ wad.js
@@ -1,49 +1,73 @@
-class Wad {
-    
-    //takes a fileobject as an argument
-    constructor() {
-        this.ident = "";
-    }
-    
-    load(file,onload){
-        var reader = new FileReader();
+console.log("wad.js");
 
+var Wad = { 
+
+    onLoad : null,
+    ident : "",
+    numlumps : -1,
+    dictpos : -1,
+    data : null,
+    lumps : [],
+
+    load : function (file) {
+        var reader = new FileReader();
+        console.log("wad.js: load");
+        
+        var self = this;
+        
         reader.onload = function(e) {
-            this.data = reader.result;
+            
+            console.log("wad.js: reader onLoad");
+            
+            self.data = reader.result;
             var i;
-            var headerReader = new DataView(this.data);
             
-            for (i = 0; i < 4; i++) this.ident += String.fromCharCode(headerReader.getUint8(i));
-            console.log(this.ident);
-            var numlumps = headerReader.getInt32(4, true);
-            var dictpos = headerReader.getInt32(4, true);
+            // header reading
+            var headerReader = new DataView(self.data);
+            for (i = 0; i < 4; i++) self.ident += String.fromCharCode(headerReader.getUint8(i));
+            self.numlumps = headerReader.getInt32(4, true);
+            self.dictpos = headerReader.getInt32(8, true);
+            
+            // dictionary reading
+            
             // the size of the dictionary is 16 * numlumps so slice that shit then create the obj
-            var dictionaryBuffer = this.data.slice(dictpos,dictpos + (numlumps * 16));
-            this.dictionary = new WadDict(dictionaryBuffer);
+            var dictionaryBuffer = self.data.slice(self.dictpos,self.dictpos + (self.numlumps * 16));
+            var dictionaryReader = new DataView(dictionaryBuffer);
             
-            if (onload != null) {
-                onload();
+            for (i = 0; i < self.numlumps; i++) {
+                p = i * 16;
+                var lumpPos = dictionaryReader.getInt32(p);
+                var lumpSize = dictionaryReader.getInt32(p + 4);
+                var lumpName = "";
+                for (j = p + 8; j < p + 16; j++) lumpName += String.fromCharCode(dictionaryReader.getUint8(j));
+                lumpEntry = { 
+                    pos : lumpPos,
+                    size : lumpSize,
+                    name : lumpName
+                }
+                self.lumps.push(lumpEntry);
             }
+            
+            
+            if (self.onLoad != null) {
+                console.log("wad.js: wad onLoad");
+                self.onLoad();
+            }
+            console.log("wad.js: after wad onLoad");
+            console.log(self.onLoad);
         }
 
-        reader.readAsArrayBuffer(file);   
-    }
-    
-}
+        reader.readAsArrayBuffer(file);  
+    },
 
-class WadDict {
-    
-    //takes a dictionary shaped arraybuffer and stores all the lump names and positions
-    constructor(data) {
-        this.lumps = [];
+    getLump : function (name) {
+        for (var i = 0; i < this.numlumps; i++) {
+            if (this.lumps[i].name == name) {
+                l = this.lumps[i];
+                return this.data.slice(l.pos,l.pos+l.size);
+            }
+        }
+        return null;
     }
-    
-}
 
-class WadDictEntry {
-    constructor(position,size,name) {
-        this.name = name;
-        this.size = size;
-        this.position = position;
-    }
-}
+};
