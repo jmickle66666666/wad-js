b0609959b9ec64b39bcef76c1c226b8d7dface49
remyabel <remyabel@gmail.com>
2017-01-17 11:45:53 -0500

    Enable sourceMap for better debugging

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		uglify: {
+			sourceMap: true,
 			build: {
 				files: {
 					'dist/wad.min.js': ['src/wad/constants.js', 'src/wad.js', 'src/wad/mapdata.js', 'src/wad/detectlump.js', 'src/wad/playpal.js', 'src/wad/colormap.js', 'src/wad/endoom.js', 'src/wad/flat.js', 'src/wad/graphic.js', 'src/wad/util.js', 'src/wad/mus2midi.js'],
