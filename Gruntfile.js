e24f49c9adc2d8ec861a69b2dd78f560014ff289
remyabel <remyabel@gmail.com>
2017-01-18 16:06:24 -0500

    Minor cleanup, readability

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,37 +2,7 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		uglify: {
-			build: {
-				options: {
-					sourceMap: true
-				},
-				files: {
-				'dist/wad.min.js':
-					[
-						'dist/parser.bundle.js',
-						'src/wad/constants.js',
-						'src/wad.js',
-						'src/wad/mapdata.js',
-						'src/wad/detectlump.js',
-						'src/wad/playpal.js',
-						'src/wad/colormap.js',
-						'src/wad/endoom.js',
-						'src/wad/flat.js',
-						'src/wad/graphic.js',
-						'src/wad/util.js',
-						'src/wad/mus2midi.js'],
-				'dist/ui.min.js':
-					['src/panels/wad_overview.js',
-						'src/panels/lump_list.js',
-						'src/panels/audio.js',
-						'src/panels/text.js',
-						'src/panels/image.js',
-						'src/panels/midi.js',
-						'src/ui.js'],
-				}	
-			}
-		},
+		
 		browserify: {
 			build: {
 				options: {
@@ -45,6 +15,7 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+
 		extract_sourcemap: {
 			build: {
 				options: {
@@ -54,12 +25,46 @@ module.exports = function(grunt) {
 					'dist': ['dist/parser.bundle.js']
 				}
 			}
+		},
+
+		uglify: {
+			build: {
+				options: {
+					sourceMap: true
+				},
+				files: {
+					'dist/wad.min.js': [
+						'dist/parser.bundle.js',
+						'src/wad/constants.js',
+						'src/wad.js',
+						'src/wad/mapdata.js',
+						'src/wad/detectlump.js',
+						'src/wad/playpal.js',
+						'src/wad/colormap.js',
+						'src/wad/endoom.js',
+						'src/wad/flat.js',
+						'src/wad/graphic.js',
+						'src/wad/util.js',
+						'src/wad/mus2midi.js'
+					]
+					'dist/ui.min.js': [
+						'src/panels/wad_overview.js',
+						'src/panels/lump_list.js',
+						'src/panels/audio.js',
+						'src/panels/text.js',
+						'src/panels/image.js',
+						'src/panels/midi.js',
+						'src/ui.js'
+					]
+				}	
+			}
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+
 	grunt.loadNpmTasks('grunt-browserify');
 	grunt.loadNpmTasks('grunt-extract-sourcemap');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	grunt.registerTask('default', ['browserify', 'extract_sourcemap', 'uglify']);
 }
