ade548fcc9c651a3fbc57827cbab7d5321254d39
remyabel <remyabel@gmail.com>
2017-01-18 12:08:01 -0500

    Install Browersify, create parser.js stub

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,14 +9,46 @@ module.exports = function(grunt) {
 					sourceMapName: 'sourceMap.map'
 				},
 				files: {
-					'dist/wad.min.js': ['src/wad/constants.js', 'src/wad.js', 'src/wad/mapdata.js', 'src/wad/detectlump.js', 'src/wad/playpal.js', 'src/wad/colormap.js', 'src/wad/endoom.js', 'src/wad/flat.js', 'src/wad/graphic.js', 'src/wad/util.js', 'src/wad/mus2midi.js'],
-					'dist/ui.min.js': ['src/panels/wad_overview.js', 'src/panels/lump_list.js', 'src/panels/audio.js', 'src/panels/text.js', 'src/panels/image.js', 'src/panels/midi.js', 'src/ui.js']
+				'dist/wad.min.js':
+					['dist/parser.bundle.js',
+						'src/wad/constants.js',
+						'src/wad.js',
+						'src/wad/mapdata.js',
+						'src/wad/detectlump.js',
+						'src/wad/playpal.js',
+						'src/wad/colormap.js',
+						'src/wad/endoom.js',
+						'src/wad/flat.js',
+						'src/wad/graphic.js',
+						'src/wad/util.js',
+						'src/wad/mus2midi.js'],
+				'dist/ui.min.js':
+					['src/panels/wad_overview.js',
+						'src/panels/lump_list.js',
+						'src/panels/audio.js',
+						'src/panels/text.js',
+						'src/panels/image.js',
+						'src/panels/midi.js',
+						'src/ui.js'],
+				}	
+			}
+		},
+		browserify: {
+			build: {
+				options: {
+					browserifyOptions: {
+						debug: true
+					},
+				},
+				files: {
+					'src/wad/parser.js': ['dist/parser.bundle.js']
 				}
 			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-browserify');
 
-	grunt.registerTask('default', ['uglify']);
+	grunt.registerTask('default', ['browserify', 'uglify']);
 }
