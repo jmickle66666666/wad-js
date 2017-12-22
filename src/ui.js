b508dd6d40a7ae410df8262d6503dbed0b06966b
jazz mickle <jaymickle@gmail.com>
2017-12-22 01:56:44 +0100

    hi

--- src/ui.js
+++ src/ui.js
@@ -14,6 +14,21 @@ var progress = 0;
 
 var wad = Object.create(Wad);
 
+function GetUrlValue(VarSearch){
+    var SearchString = window.location.search.substring(1);
+    var VariableArray = SearchString.split('&');
+    for(var i = 0; i < VariableArray.length; i++){
+        var KeyValuePair = VariableArray[i].split('=');
+        if(KeyValuePair[0] == VarSearch){
+            return KeyValuePair[1];
+        }
+    }
+}
+
+if (GetUrlValue("wad") != null) {
+	wad.loadURL(GetUrlValue("wad"));
+}
+
 function initWad() {
 	self.errormsg = null;
 	$(errors).html("");
