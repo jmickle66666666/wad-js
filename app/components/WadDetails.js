445c4855b3352e5f7d58197cc73a1739644c948d
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-03-07 21:51:37 -0800

    adjust UI

--- app/components/WadDetails.js
+++ app/components/WadDetails.js
@@ -5,8 +5,6 @@ import style from './WadDetails.scss';
 export default ({ selectedWad: wad }) => (
     <div className={style.wadDetailsOuter}>
         <h2 className={style.wadDetailsTitle}>
-            Lumps for
-            {' '}
             {wad.name}
         </h2>
         <div className={style.wadDetailsInner} />
