086596ec5eb0b205a8532b9d91fc43ade46ecb46
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-03-10 22:09:52 -0700

    upload date

--- app/components/WadMetadata.js
+++ app/components/WadMetadata.js
@@ -1,24 +1,33 @@
 import React from 'react';
+import moment from 'moment';
 
 import style from './WadMetadata.scss';
 
 import Help from './Help';
 import ErrorMessageList from './ErrorMessageList';
 
-export default ({ wad, updateFilename }) => (
+export default ({
+    wad,
+    updateFilename,
+    focusOnWad,
+}) => (
     <div className={style.wadMetadataOuter}>
         <Help id="wad-metadata" title="the metadata panel">
-            <h3 className={style.wadMetadataTitle}>
-                Metadata
+            <h3 className={style.wadMetadataTitle} onClick={focusOnWad}>
+                    Metadata
             </h3>
         </Help>
         <div className={style.wadMetadataInner}>
             <ErrorMessageList errors={wad.errors} />
             <div className={style.wadMetadataTable}>
+                <div className={style.wadMetadataEntry}>
+                    <div className={style.wadMetadataLabel}>Uploaded:</div>
+                    <div className={style.wadMetadataValue}>{moment(wad.uploadEndAt).format('M/D/YYYY h:mm a')}</div>
+                </div>
                 <label htmlFor="filename" className={style.wadMetadataEntry}>
                     <div className={style.wadMetadataLabel}>
-                        Filename:
-                    </div>
+                            Filename:
+                        </div>
                     <input
                         id="filename"
                         className={style.wadMetadataValue}
