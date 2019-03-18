c39f4bd5126092ea0414af112bca226b6254bc96
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-03-18 16:53:18 -0700

    rename LumpTypes folder to Lumps

--- app/components/WadLumpDetailDisplay.js
+++ app/components/WadLumpDetailDisplay.js
@@ -1,66 +1,64 @@
 import React from 'react';
 
-import imageStyle from './LumpTypes/ImageLump.scss';
-
-import Palettes from './LumpTypes/Palettes';
-import Colormaps from './LumpTypes/Colormaps';
-import PatchNames from './LumpTypes/PatchNames';
-import TextureNames from './LumpTypes/TextureNames';
-import ImageLump from './LumpTypes/ImageLump';
-import Texture from './LumpTypes/Texture';
+import Palettes from './Lumps/Palettes';
+import Colormaps from './Lumps/Colormaps';
+import PatchNames from './Lumps/PatchNames';
+import TextureNames from './Lumps/TextureNames';
+import ImageLump from './Lumps/ImageLump';
+import Texture from './Lumps/Texture';
 
 export default ({ wad, lump }) => {
     switch (lump.type) {
-    default: {
-        return null;
-    }
-    case 'palettes': {
-        return (
-            <Palettes lump={lump} />
-        );
-    }
-    case 'colormaps': {
-        return (
-            <Colormaps wad={wad} lump={lump} />
-        );
-    }
-    case 'flats': {
-        return (
-            <ImageLump wad={wad} lump={lump} />
-        );
-    }
-    case 'patches': {
-        if (lump.isPNAMES) {
+        default: {
+            return null;
+        }
+        case 'palettes': {
             return (
-                <PatchNames lump={lump} />
+                <Palettes lump={lump} />
             );
         }
-        return (
-            <ImageLump wad={wad} lump={lump} />
-        );
-    }
-    case 'textures': {
-        if (lump.isTEXTUREx) {
+        case 'colormaps': {
             return (
-                <TextureNames lump={lump} />
+                <Colormaps wad={wad} lump={lump} />
             );
         }
-        return (
-            <Texture wad={wad} lump={lump} />
-        );
-    }
-    case 'sprites': {
-        return (
-            <ImageLump wad={wad} lump={lump} />
-        );
-    }
-    case 'menu': {
-        return (
-            <ImageLump
-                wad={wad}
-                lump={lump}
-            />
-        );
-    }
+        case 'flats': {
+            return (
+                <ImageLump wad={wad} lump={lump} />
+            );
+        }
+        case 'patches': {
+            if (lump.isPNAMES) {
+                return (
+                    <PatchNames lump={lump} />
+                );
+            }
+            return (
+                <ImageLump wad={wad} lump={lump} />
+            );
+        }
+        case 'textures': {
+            if (lump.isTEXTUREx) {
+                return (
+                    <TextureNames lump={lump} />
+                );
+            }
+            return (
+                <Texture wad={wad} lump={lump} />
+            );
+        }
+        case 'sprites': {
+            return (
+                <ImageLump wad={wad} lump={lump} />
+            );
+        }
+        case 'menu': {
+            return (
+                <ImageLump
+                    wad={wad}
+                    lump={lump}
+                />
+            );
+        }
     }
 };
