75488689e7a0f77b809d309fdc13553ecbbae40e
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-04-02 21:01:11 -0700

    Update SettingsMenu.js

--- app/components/SettingsMenu.js
+++ app/components/SettingsMenu.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 import style from './SettingsMenu.scss';
 
+import Help from './Help';
+
 export default ({ settings, handleSettingChange, toggleSettingsMenu }) => (
     <div className={style.settingsMenuOuter}>
+        <Help id="settings" title="settings">
         <h2
             role="button"
             className={style.settingsMenuTitle}
@@ -11,6 +14,7 @@ export default ({ settings, handleSettingChange, toggleSettingsMenu }) => (
             onKeyPress={toggleSettingsMenu}
             tabIndex={0}
         >
+        </Help>
             Settings
         </h2>
         <label className={style.setting} htmlFor="playbackLoop">
