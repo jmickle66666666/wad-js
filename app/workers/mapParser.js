819cfde3621ad3d8ba601a6adbaf4a39f305ebf5
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-04-05 17:06:39 -0700

    boilerplate for mapParser worker

---
+++ app/workers/mapParser.js
@@ -0,0 +1,17 @@
+import {
+} from '../lib/constants';
+
+onmessage = (message) => {
+    const { wadId, lumpId, data } = message.data;
+
+    console.log(`Parsing map '${lumpId}' (WAD: '${wadId}') ...`);
+
+    const mapData = {};
+
+
+    postMessage({
+        wadId,
+        lumpId,
+        mapData,
+    });
+};
