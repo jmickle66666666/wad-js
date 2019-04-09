6ce91a2e0c96be709b6868d48896d28880a00300
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-04-09 00:05:14 -0700

    ANSI screen parser

--- app/workers/textConverter.js
+++ app/workers/textConverter.js
@@ -1,7 +1,59 @@
 import {
     ANSI_LUMPS,
     INVALID_TEXT,
+    ANSI_BACKGROUND_COLOR_CODES,
+    ANSI_FOREGROUND_COLOR_CODES,
+    ANSI_CODE_PAGE_437_TABLES,
 } from '../lib/constants';
+
+const interpolateBackgroundAnsiCode = int => (
+    int === Number.isNaN ? null : ANSI_BACKGROUND_COLOR_CODES[int]
+);
+
+const interpolateForegroundAnsiCode = int => (
+    int === Number.isNaN ? null : ANSI_FOREGROUND_COLOR_CODES[int]
+);
+
+const parseAnsiScreen = (data) => {
+    const blocks = [];
+    let line = [];
+
+    for (let i = 0; i <= data.byteLength; i += 2) {
+        if (i !== 0 && i % 160 === 0) {
+            blocks.push(line);
+            line = [];
+        }
+
+        // last byte is unimportant but necessary to push the last line
+        if (i === data.byteLength) {
+            break;
+        }
+
+        const characterByte = data.getUint8(i);
+        const character = ANSI_CODE_PAGE_437_TABLES[characterByte];
+
+        const colorByte = data.getUint8(i + 1);
+
+        // missings bits are replaced by zeroes
+        const colorBits = (`000000000${colorByte.toString(2)}`).substr(-8);
+
+        const foregroundBits = colorBits.slice(4, 8);
+        const foregroundInt = parseInt(foregroundBits, 2);
+        const foreground = interpolateForegroundAnsiCode(foregroundInt);
+
+        const backgroundBits = colorBits.slice(0, 4);
+        const backgroundInt = parseInt(backgroundBits, 2);
+        const background = interpolateBackgroundAnsiCode(backgroundInt);
+
+        line.push({
+            character,
+            foreground,
+            background,
+        });
+    }
+
+    return blocks;
+};
 
 const decodeText = data => decodeURI(new TextDecoder('utf-8').decode(data).replace(/\u0000/g, ' '));
 
@@ -13,17 +65,12 @@ onmessage = (message) => {
     console.log(`Converting '${lumpId}' to text (WAD: '${wadId}') ...`);
 
     if (ANSI_LUMPS.includes(lumpId)) {
-        console.log(`'${lumpId}' is an ANSI screen (WAD: '${wadId}').`);
-
-
-        // TODO: convert bytes to a renderable object;
-
-        const text = '';
+        const screenData = parseAnsiScreen(data);
 
         postMessage({
             wadId,
             lumpId,
-            text,
+            text: screenData,
         });
 
         return;
@@ -49,8 +96,6 @@ onmessage = (message) => {
             text: null,
             error,
         });
-
-        return;
     }
 
     const splitText = processNewLines(decodedText);
