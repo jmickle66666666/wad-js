a7728d088358b77a889c3dcfad472c13421822c3
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-04-09 12:05:55 -0700

    allow to start without branding

--- app/components/Logo.js
+++ app/components/Logo.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import style from './Logo.scss';
 
 import archie from '../assets/archie.png';
+
+import { NO_BRAND_ENV } from '../lib/constants';
 
 export default ({
     image,
@@ -11,10 +13,17 @@ export default ({
     customJSStyle,
 }) => (
     <a className={customLogoStyle || style.logo} href="#uploader">
-        {image === false ? null : <img alt="archie" src={archie} />}
-        <h1>
-            <span className={customWadStyle || style.wad}>wad</span>
-            <span className={customJSStyle || style.js}>js</span>
-        </h1>
+        {(TARGET !== NO_BRAND_ENV
+                && (
+                    <Fragment>
+                        {image === false ? null : <img alt="archie" src={archie} />}
+                        <h1>
+                            <span className={customWadStyle || style.wad}>wad</span>
+                            <span className={customJSStyle || style.js}>js</span>
+                        </h1>
+                    </Fragment>
+                ))
+                || null
+        }
     </a>
 );
