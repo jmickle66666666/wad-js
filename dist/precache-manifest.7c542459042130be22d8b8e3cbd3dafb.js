d02fc8e3cd89fd7465b1b1c9a52d760b60cd403b
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-04-09 23:02:51 -0700

    new build

---
+++ dist/precache-manifest.7c542459042130be22d8b8e3cbd3dafb.js
@@ -0,0 +1,25 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "513c1c80d88dc0ae192467e0fccf6907",
+    "url": "../index.html"
+  },
+  {
+    "url": "main.fc2f248b.js"
+  },
+  {
+    "revision": "52b4b6943c9d96e47b82c2539d4d39d8",
+    "url": "mapParser.f809f043.js"
+  },
+  {
+    "revision": "29ee1157bd4ff68d9a642bfad55400ef",
+    "url": "midiConverter.39bd95a1.js"
+  },
+  {
+    "revision": "b5b2bd974170d9af554bc989c79ef01c",
+    "url": "simpleImageConverter.548114e2.js"
+  },
+  {
+    "revision": "027ced437afd309f31603ef888e500e5",
+    "url": "textConverter.59523b90.js"
+  }
+]);
