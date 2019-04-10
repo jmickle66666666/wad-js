ab582c56f3a93ef221ed9e59e477493e138d8471
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-04-10 00:01:01 -0700

    path to service worker should be relative

--- app/components/App.js
+++ app/components/App.js
@@ -118,7 +118,7 @@ export default class App extends Component {
                 });
             } else {
                 window.addEventListener('load', () => {
-                    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+                    navigator.serviceWorker.register('./service-worker.js').then((registration) => {
                         console.log('SW registered: ', registration);
                     }).catch((registrationError) => {
                         console.log('SW registration failed: ', registrationError);
