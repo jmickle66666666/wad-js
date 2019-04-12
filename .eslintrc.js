531fd5a275be56496ec40330e2c232e2adea4ace
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-04-11 20:25:57 -0700

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,11 +12,10 @@ module.exports = {
         "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
         "react/jsx-indent": ["error", 4],
         "react/jsx-indent-props": ["error", 4],
-        "indent": ["error", 4],
+        "indent": ["error", 4, { "SwitchCase": 1 }],
         "no-plusplus": 0,
         "class-methods-use-this": 0,
         "react/sort-comp": 0,
         "no-bitwise": 0,
     },
-    parser: "babel-eslint"
 };
