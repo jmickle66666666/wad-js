0d68c73bbf4f67d1f7b2188ef179394101810c2e
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-04-26 16:00:04 -0700

    add sampleRate to lump model

--- app/models/Lump.js
+++ app/models/Lump.js
@@ -17,9 +17,10 @@ export default class Lump {
         height,
         xOffset,
         yOffset,
+        sampleRate,
+        lineIndex,
         count,
         data,
-        lineIndex,
     }) {
         this.name = name;
         this.description = description;
@@ -32,9 +33,10 @@ export default class Lump {
         this.height = height;
         this.xOffset = xOffset;
         this.yOffset = yOffset;
+        this.sampleRate = sampleRate;
+        this.lineIndex = lineIndex;
         this.count = count;
         this.data = data;
-        this.lineIndex = lineIndex;
     }
 
     get sizeInBytes() {
