e1796dd52154dbede38f79347d85a600c782404e
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-04-28 00:14:52 -0700

    remove console.log

--- app/components/Lumps/Map.js
+++ app/components/Lumps/Map.js
@@ -2,29 +2,26 @@ import React, { Fragment } from 'react';
 
 import style from './ImageLump.scss';
 
-export default ({ wad, lump }) => {
-    console.log({ lump });
-    return (
-        <Fragment>
-            {lump.data && (
-                <div className={style.wadLumpDetailsEntry}>
-                    Map lumps:
-                    {' '}
-                    {Object.keys(lump.data || {}).map(lumpName => lumpName).join(', ')}
-                </div>
-            )}
+export default ({ wad, lump }) => (
+    <Fragment>
+        {lump.data && (
             <div className={style.wadLumpDetailsEntry}>
-                Things:
+                Map lumps:
+                {' '}
+                {Object.keys(lump.data || {}).map(lumpName => lumpName).join(', ')}
             </div>
-            <div className={style.wadLumpDetailsEntry}>
-                Sectors:
-            </div>
-            <div className={style.wadLumpDetailsEntry}>
-                Linedefs:
-            </div>
-            <div className={style.wadLumpDetailsEntry}>
-                Vertices:
-            </div>
-        </Fragment>
-    );
-};
+        )}
+        <div className={style.wadLumpDetailsEntry}>
+            Things:
+        </div>
+        <div className={style.wadLumpDetailsEntry}>
+            Sectors:
+        </div>
+        <div className={style.wadLumpDetailsEntry}>
+            Linedefs:
+        </div>
+        <div className={style.wadLumpDetailsEntry}>
+            Vertices:
+        </div>
+    </Fragment>
+);
